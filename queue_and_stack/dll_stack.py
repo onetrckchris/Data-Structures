d2812faba90c35424c163c6bb92ad38725952f15
onetrckchris <onetrckchris@gmail.com>
2019-11-11 15:57:43 -0800

    have not finished MVP. Still working on the pop and dequeue methods

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,30 @@
-from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        # print("This is the tail: ", self.storage.tail.value)
+        # print("This is the head: ", self.storage.head.value)
 
     def pop(self):
-        pass
+        if self.storage.head:
+            self.storage.remove_from_head()
+        # print("This is the tail I'm trying to pop(): ", self.storage)
 
     def len(self):
-        pass
+        return self.storage.length
+
+test_stack = Stack()
+test_stack.push(5)
+test_stack.push(10)
+test_stack.push(52)
+test_stack.pop()
+test_stack.pop()
+print("Test stack: ", test_stack.storage)
